8119b0f85d9bf4405a7786104a954afd098a1f20
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-03 00:08:26 -0500

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// let's go!
+require('dotenv').config({ path: 'variable.env' });
+const createServer = require('./createServer');
+// const db = require('./db');
+
+const server = createServer();
+
+server.start({
+  credential: true,
+  cors: process.env.FRONTEND_URL,
+}, (deets) => {
+  // eslint-disable-next-line
+  console.log(`Server is running on port http://localhost:${deets.port}`);
+});
