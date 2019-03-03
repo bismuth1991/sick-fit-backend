6386ec6089038ec1043e3a338c6d28d0a374f970
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-03 01:12:00 -0500

    create resolver for items Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,12 @@
-const Query = {
+const { forwardTo } = require('prisma-binding');
 
+/* eslint-disable no-unused-vars */
+const Query = {
+  items: forwardTo('db'),
+  // items: async (parent, args, ctx, info) => {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
 };
 
 module.exports = Query;
